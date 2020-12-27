78c2746428b685a47a352f609c34f9f29a6f34ee
deepyes02 <deepyes@outlook.com>
2020-12-27 13:12:36 +0545

    fully working ajax search implementation

--- ajax_search_results.php
+++ ajax_search_results.php
@@ -2,17 +2,49 @@
 
 if(!defined('WP_PLUGIN_URL')){
     require_once(realpath('../../../').'/wp-config.php');
-
 }
 // print_r(get_declared_classes());
-
 if (empty($_GET['s'])){
+    echo "Please add <?s>=<string> to search for the term you want";
     exit;
 }
-$max_posts = 3;
+$max_posts = 10;
 $wp_query = new WP_Query();
 $wp_query->query(array('s'=>$_GET['s'], 'showposts'=>$max_posts));
-
-foreach($wp_query->posts as $result){
-    print_r($result);
+//if there are no results
+if(!count($wp_query->posts)){
+    print file_get_contents('templates/no_results.tpl');
+    exit;
 }
+
+//otherwise show the results;
+
+$container = array('content'=>'');//define container's only placeholder
+$single_tpl = file_get_contents('templates/single_result.tpl');
+foreach($wp_query->posts as $result){
+    // echo($result->post_title);
+    $result->permalink = get_permalink($result->ID);
+    $container['content'] .= parse($single_tpl, $result);
+}
+
+//wrap the results
+$result_container_tpl = file_get_contents('templates/results_container.tpl');
+print parse($result_container_tpl, $container);
+
+/**
+ * parse
+ * a simple parsing function for basic templating
+ * 
+ * @param $tpl string A formatting string containing [+placeholders+];
+ * @param $hash array An associative array containing keys and values, eg array('key'=>'value')
+ * @return string Placeholdders corresponding to the keys of the has will be replaced with the
+ * values the resulting string will be returned to
+ */
+
+ function parse($tpl, $hash){
+     foreach ($hash as $key => $value){
+         $tpl = str_replace('[+'.$key.'+]', $value, $tpl);
+     }
+     return $tpl;
+ }
+
--- includes/live-search.js.php
+++ includes/live-search.js.php
@@ -5,13 +5,11 @@
  * with a valid url, lets see what this is doing
  */
 header('Content-type:text/javascript');
-include_once('../includes/class-live-search.php');
+include_once('./class-live-search.php');
 $plugin_url = plugins_url();
-$search_handler = $plugin_url . '/ajax_search_results.php';
-
+$search_handler = $plugin_url . '/live-search/ajax_search_results.php';
 ?>
 <script>
-
 jQuery(document).ready(main);
 
 function main(){
@@ -21,21 +19,28 @@ function main(){
 }
 
 function get_search_results(){
+    // console.log(search_letter.innerText);
+    let search_letter = document.querySelector('.search-form label');
+    search_letter.innerText = "Searching...";
     let search_query = jQuery('#search-form-1').val();
     if(search_query != "" && search_query.length > 2) {
-        jQuery.get("./ajax_search_results.php", {s:search_query}, write_results_to_page );
+        search_letter.innerText = "Searching...";
+        let mydumper = jQuery.get('/wp-content/plugins/live-search/ajax_search_results.php', {s:search_query}, write_results_to_page );
+        // console.log(mydumper);
     } else {
+        search_letter.innerText = "Type at least 3 letter to search...";
         console.log('Search term empty or too short');
     }
 }
 
-function write_results_to_page(){
+function write_results_to_page(data,status, xhr){
+    let search_letter = document.querySelector('.search-form label');
     if (status == 'error'){
         let msg = 'Sorry but there was an error';
         console.error(msg + xhr.status + " " + xhr.statusText);
     } else {
         jQuery('#ajax_search_results_go_here').html(data);
+        search_letter.innerText = "Search Results";
     }
 }
-
 </script>
